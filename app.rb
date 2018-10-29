aa5a98d9d98ef8f8f5f62724764dfd50141cbfd0
Robert Schulte <schulte.robertw@outlook.com>
2018-10-29 16:54:04 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,19 @@
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		erb :date
 	end
 
 
 end
+
+
+# For each of the following examples, create a .erb file in the views directory and a route in app.rb which renders that template. Make sure each template contains the requested content.
